b530a25301e955651ffe20645849e2579c83b0e4
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-24 23:26:40 -0800

    updated header

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
-import "./Header";
+import Header from "./Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
