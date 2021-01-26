0e915ad85b1f7683c8887f0533c7f6b5acf4cb53
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-26 01:50:05 -0800

    added sass

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
-import classes from "./header.module.css";
+import classes from "./header.module.scss";
 
 const Header = () => (
-  <header class={classes.Center}>
+  <header class={classes.Headerstyles}>
     <h1>Discover hidden places in the world around you</h1>
     <a href="/" class={classes.Aqua}>
       Download Excursion (Coming soon!)
