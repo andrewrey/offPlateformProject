9a970fe143f012873d1d9feb80044a20e5a8ae62
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-26 12:03:58 -0800

    added remaning images to main component with basic styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,30 @@
 import classes from "./Main.module.scss";
+import video from "../../assets/video/excursion.mp4";
 
 const Main = () => (
   <main className={classes.Main}>
-    <video
-      src="./../../assets/video/roadVideo.mp4"
-      autoPlay
-      loop
-      controls
-      typeof="movie/mp4"
-    >
+    <video src={video} autoPlay loop controls typeof="movie/mp4">
       Your Browser does not support this video
     </video>
+    <h2>Your personal travel guide</h2>
+    <p>
+      Excursion remembers places you like, and recommends new points of interst
+      around you.
+    </p>
+    <figure>
+      <img
+        src="https://content.codecademy.com/programs/freelance-one/excursion/images/camp.jpg"
+        alt="yellow tent at night with ambient light"
+      />
+    </figure>
+    <figure>
+      <img
+        src="https://content.codecademy.com/programs/freelance-one/excursion/images/phone.png"
+        alt="phone icon"
+      />
+    </figure>
+    <h2>Coming Soon for iPhone and Android</h2>
+    <a href="/">Downbload Excursion (Coming soon!)</a>
   </main>
 );
 
